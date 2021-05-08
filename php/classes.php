32e22109dcea3503daf98410abb196e813a15859
Hazem <hazemhagaarf@gmail.com>
2021-05-07 18:00:26 -0700

    intial commit

--- php/classes.php
+++ php/classes.php
@@ -44,4 +44,86 @@ class reservation extends Database_Handler{
     return mysqli_insert_id($conn);
   }
 }
+
+class client extends Database_Handler{
+  public $id;
+  public $first_name;
+  public $last_name;
+  public $nationality;
+  public $mobile;
+  public $email;
+  public $company;
+  function __construct($id){
+    $this->id=$id;
+  }
+  function insert($fields){
+
+  }
+
+  function getbyID($id){
+    $SQL="select * from client where ID=$id";
+    $conn=parent::create_connection();
+    $result=mysqliquery($conn,$SQL);
+    $row=mysqli_fetch_array($result);
+    $this-> id=$row[0];
+    $this->first_name=$row[1];
+    $this->last_name=$row[2];
+    $this->nationality=$row[3];
+    $this->mobile=$row[4];
+    $this->email=$row[5];
+    $this->company=$row[6];
+    parent::close_connection($conn);
+  }
+
+  function insertintoDB($id,$first_name,$last_name,$nationality,$mobile,$email,$company){
+    $this-> id=$id;
+    $this->first_name=$first_name;
+    $this->last_name=$last_name;
+    $this->nationality=$nationality;
+    $this->mobile=$mobile;
+    $this->email=$email;
+    $this->company=$company;
+
+  }
+}
+
+abstract class User
+{
+  public $id, $name, $username, $password, $position;
+  abstract function change_password($password,$Cpassword,$Opassword);
+  abstract function login($username,$password);
+}
+
+class Front_Office extends User{
+  function create_reservation($client_id,$room_no,$guest_names,$guest_count,$arrival,$departure,$comments){
+      $reservation=new reservation($guest_names,$guest_count,$arrival,$departure,$comments);
+
+     $fields = [
+          "client_id" => $client_id,
+          "room_no"=>$room_no
+      ];
+      $reservation->insert($fields);
+  }
+
+  function cancel_reservation($id){
+
+  }
+
+  function edit_reservation($id){
+
+  }
+
+  function create_malfunction_entry($description,$isArchived,$entry_by,$string){
+
+  }
+
+  function create_electricity_followup_entry($reading,$photo){
+
+  }
+
+  function create_water_followup_entry($reading,$photo){
+  }
+}
+
+
 ?>
---
+++ reservation.php
@@ -0,0 +1,20 @@
+<html>
+<form method="post">
+<input type='text' name='guest_names'>
+<input type='text' name='guest_count'>
+<input type='text' name='arrival'>
+<input type='text' name='departure'>
+<input type='text' name='comments'>
+<input type="submit"  name="submit" ></input>
+</form>
+</html>
+
+<?php
+include "classes.php";
+$user=new Front_Office();
+if(isset($_POST["submit"])){
+$user->create_reservation(50,101,$_POST['guest_names'],$_POST['guest_count'],$_POST['arrival'],$_POST['departure'],$_POST['comments']);
+
+
+}
+?>
